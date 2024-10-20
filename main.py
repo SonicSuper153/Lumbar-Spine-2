bb39756128fac7785241435b86a17b25956d76e6
Rakesh Arora <mannyisha808@gmail.com>
2024-10-20 13:38:29 +0530

    frontend added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 from torchvision import transforms
 import torchvision
 from torch import nn
+import torch.nn.functional as F
 
 from flask import Flask, request, render_template
 app = Flask(__name__)
@@ -26,7 +27,20 @@ model.classifier = torch.nn.Sequential(
               out_features=len(class_names),
               bias=True).to(device))
 
-model.load_state_dict(torch.load('model_final_weights.pth'))
+model.load_state_dict(torch.load('model_weights.pth',weights_only=True))
+
+
+model2 = torchvision.models.efficientnet_b0(weights=weights).to(device)
+for param in model2.features.parameters():
+    param.requires_grad = False
+
+model2.classifier = torch.nn.Sequential(
+    nn.Dropout(p=0.2, inplace=True),
+    nn.Linear(in_features=1280, 
+              out_features=2,
+              bias=True).to(device))
+
+model2.load_state_dict(torch.load('model_Spine_Or_No.pth',weights_only=True))
 
 
 def predict_image_class(image_path, model, class_names, device='cpu'):
@@ -42,15 +56,41 @@ def predict_image_class(image_path, model, class_names, device='cpu'):
     input_batch = input_tensor.unsqueeze(0)
 
     input_batch = input_batch.to(device)
+
     model.eval()
 
     with torch.no_grad():
         output = model(input_batch)
+
         _, predicted_idx = torch.max(output, 1)
 
     predicted_class = class_names[predicted_idx.item()]
+
     return predicted_class
 
+def predict_image_class2(image_path, model ,device='cpu'):
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    image = Image.open(image_path).convert('RGB')
+    tensor = preprocess(image)
+    batch = tensor.unsqueeze(0)
+    
+    batch = batch.to(device)
+    model.eval()
+    
+    with torch.no_grad():
+        output = model(batch)
+        probabilities = F.softmax(output, dim=1)
+        
+        _, predicted_idx = torch.max(output, 1)
+        confidence = probabilities[0][predicted_idx].item() * 100  
+    
+    return predicted_idx.item(), confidence
 
 @app.route("/")
 def home():
@@ -61,7 +101,9 @@ def predict():
   if 'image' not in request.files:
         return "No image is uploaded !"
   file = request.files["image"]
-  file.save("static/input.jpg");
-  return render_template("output.html", result=predict_image_class('static/input.jpg', model, class_names, device='cpu'))
+  file.save("static/input.jpg")
+  if(predict_image_class2('static/input.jpg', model2, device='cpu')[0] == 0):
+      return render_template("error.html")
+  return render_template("output.html", result=predict_image_class('static/input.jpg', model,class_names, device='cpu'))
 
 app.run()
